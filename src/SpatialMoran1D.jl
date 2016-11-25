4f4083d62cbeecf0a998214cc6d27d7e1a1741b9
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2016-11-25 09:58:17 +0100

    * Extended DocStrings

--- src/SpatialMoran1D.jl
+++ src/SpatialMoran1D.jl
@@ -15,12 +15,69 @@ module SpatialMoran1D
 
     using Distributions
 
-    TKomaP(k,n)   = n^3/pi * (1/(k-1)^2-1/k^2)*(1-1/k) + (1-2/pi)
-    TKomaP(k,n,m) = n^3/pi *(1/(k-m)^2-1/(k+1-m)^2)*((k-m)/(k-m+1))+(1-2/pi)
-    TKomaErfP(k,n,C0::Real=1.) = 1/4/C0^2*(1/erfinv((k-1)/n)^2-1/erfinv(k/n)^2)*(1-1/k)*n
+    """
+        ETKomaP(k, n)
+
+    Expected value for the kth coalescent time under the spatial model with PBC.
+    # Arguments
+    * `k`: Lineages present
+    * `n`: Population size
+    """
+    TKomaP(k, n)   = n^3/pi * (1/(k-1)^2-1/k^2)*(1-1/k) + (1-2/pi)
+    """
+        ETKomaP(k, n, m)
+
+    Expected value for the kth coalescent time under the spatial model with PBC.
+    # Arguments
+    * `k`: Lineages present
+    * `n`: Population size
+    * `m`: Tweak parameter
+    """
+    TKomaP(k, n, m) = n^3/pi *(1/(k-m)^2-1/(k+1-m)^2)*((k-m)/(k-m+1))+(1-2/pi)
+    """
+        ETKomaErfP(k, n, C0::Real=1.)
+
+    Expected value for the kth coalescent time under the spatial model with PBC.
+    Exact solution via inverse of error function.
+    # Arguments
+    * `k`: Lineages present
+    * `n`: Population size
+    * `C0=1.0`: Initial concentration; 1.0 is fully occupied.
+    """
+    ETKomaErfP(k, n, C0::Real=1.) = 1/4/C0^2*(1/erfinv((k-1)/n)^2-1/erfinv(k/n)^2)*(1-1/k)*n
+    """
+        ETotalKoma(n)
+
+    Expected value for total length of a genealogy under the spatial model.
+    # Arguments
+    * `n`: Population size
+    """
     ETotalKoma(n) = sum( k->TKomaP(k,n)*k, 2:n )
+    """
+        ETMRCAKoma(n)
+
+    Expected value for time to the MRCA under the spatial model.
+    # Arguments
+    * `n`: Population size
+    """
     ETMRCAKoma(n) = sum( k->TKomaP(k,n), 2:n )
+    """
+        ESegSitesKoma(n, μ)
+
+    Expected value for the number of segregating sites under the spatial model.
+    # Arguments
+    * `n`: Population size
+    * `μ`: Mutation rate
+    """
     ESegSitesKoma(n, μ) = μ*ETotalKoma(n)
+    """
+        EPiKoma(n, μ)
+
+    Expected value for the number of pairwise differences under the spatial model.
+    # Arguments
+    * `n`: Population size
+    * `μ`: Mutation rate
+    """
     EPiKoma(n, μ) = μ/12*n^2*(n+1)
 
     """
